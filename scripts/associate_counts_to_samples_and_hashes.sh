8a8c408aa5cbebb96250b98e96c2569fd1d00101
David Koslicki <dmkoslicki@gmail.com>
2025-09-29 12:22:24 -0400

    tweak memory for currently available #4

--- scripts/associate_counts_to_samples_and_hashes.sh
+++ scripts/associate_counts_to_samples_and_hashes.sh
@@ -5,7 +5,7 @@ python logan_hash_tools.py prepare-hashes \
   --hash-parquet-root /scratch/dmk333_new/unclassified_frequent_Logan_metagenome_hashes/data/anti_join_parquets/logan_metagenomes_minus_rights_k31_20250925_141847 \
   --hash-glob "bucket=*/data_*.parquet" \
   --working-duckdb /scratch/dmk333_new/unclassified_frequent_Logan_metagenome_hashes/data/logan_work.duckdb \
-  --threads 128 --memory-limit "3500GB" --temp-dir /scratch/duck_tmp \
+  --threads 128 --memory-limit "2000GB" --temp-dir /scratch/duck_tmp \
   --recreate-hashes --deduplicate-hashes --overwrite
 
 
