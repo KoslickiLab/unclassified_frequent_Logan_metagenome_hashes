c8bac3245c07eb4bcc8085d22d8f131f75606e5a
David Koslicki <dmkoslicki@gmail.com>
2025-10-01 16:12:26 -0400

    add some helper doc about the scripts #2

--- scripts/get_frequent_hashes.sh
+++ scripts/get_frequent_hashes.sh
@@ -9,7 +9,7 @@ python logan_hash_tools.py percentiles \
   --percentiles $BOTTOMP $TOPP \
   --counts-path /scratch/dmk333_new/unclassified_frequent_Logan_metagenome_hashes/data/hash_and_sample_id_counts/hash_counts.parquet \
   --emit-mapping-for-hashes \
-  --map-out /scratch/dmk333_new/unclassified_frequent_Logan_metagenome_hashes/data/hash_and_sample_id_counts/hash_counts_${BOTTOMP}_${TOPP}/hash_pct_${BOTTOMP}_${TOPP}_pairs \
+  --map-out /scratch/dmk333_new/unclassified_frequent_Logan_metagenome_hashes/data/hash_and_sample_id_counts/hash_counts_${BOTTOMP}_${TOPP}/hash_pct_${BOTTOMP}_${TOPP}_pairs_2 \
   --attach-db /scratch/shared_data_new/Logan_yacht_data/processed_data/database_all.db \
   --db-schema sigs_dna --db-table signature_mins --attach-alias ro \
   --ksize 31 \
