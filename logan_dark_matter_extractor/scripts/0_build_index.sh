1503a9cf77e5c350c5023d5958c724e2258f8a83
David Koslicki <dmkoslicki@gmail.com>
2025-10-08 15:51:50 -0400

    change to correct directory #6

--- logan_dark_matter_extractor/scripts/0_build_index.sh
+++ logan_dark_matter_extractor/scripts/0_build_index.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd /scratch/dmk333_new/unclassified_frequent_Logan_metagenome_hashes
 python -m logan_darkmatter.cli build-index \
   --parquet /scratch/dmk333_new/unclassified_frequent_Logan_metagenome_hashes/data/hash_and_sample_id_counts/hash_counts_25_10000/filtered_hashes_no_Serratus.parquet \
   --index-root /scratch/logan_darkmatter/novel_index \
--- logan_dark_matter_extractor/scripts/1_annotate_contigs_from_accession.sh
+++ logan_dark_matter_extractor/scripts/1_annotate_contigs_from_accession.sh
@@ -4,6 +4,7 @@
 # slow. See: https://matrix.to/#/!gsCtamVSdOOWwDWBIT:gitter.im/$IQgvkLQ1THJX862CgvaFQqIABdHl7-CJ5VTPOZB8txw?via=gitter.im&via=matrix.org&via=matrix.freyachat.eu
 # at: https://app.gitter.im/#/room/#sourmash-bio_community:gitter.im Message in a reply at 10/8/2025 to a thread
 # started 10/7/2025
+cd /scratch/dmk333_new/unclassified_frequent_Logan_metagenome_hashes
 python -m logan_darkmatter.cli annotate \
   --index-root /scratch/logan_darkmatter/novel_index \
   --workdir /scratch/logan_darkmatter/work \
